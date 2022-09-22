b2c234f39af37a859c9335809920cb8a0de8fcc4
magnovicentini <magno.vicentini@mercadolivre.com>
2022-09-22 17:28:42 -0300

    terminando exercicios

--- src/AulaPratica/TempCities.java
+++ src/AulaPratica/TempCities.java
@@ -6,10 +6,25 @@ public class TempCities {
                 "Asunción", "São Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
         int[][] tempCities = { { -2, 33}, {-3, 32}, {-8, 27}, {4, 37}, {6, 42}, {5, 43}, {0, 39}, {-7, 26}, {-1, 31}, {-10, 35} };
 
-        System.out.println(cities[0]);
+        System.out.println(tempCities[5][1]);
+
+        String hottestCityName = cities[0];
+        String collestCityName = cities[0];
+
+        int hottestCityTemp = 0;
+        int collestCityTemp = 0;
 
         for (int i = 0; i < cities.length; i++) {
-            int hottestCity = [i]
+            if ( hottestCityTemp < tempCities[i][1]) {
+                hottestCityTemp = tempCities[i][1];
+                hottestCityName = cities[i];
+            }
+            if ( collestCityTemp > tempCities[i][0]) {
+                collestCityTemp = tempCities[i][0];
+                collestCityName = cities[i];
+            }
         }
+        System.out.println("A cidade que registrou maior temperatura foi "+ hottestCityName + " com a temperatura de " + hottestCityTemp);
+        System.out.println("A cidade que registrou menor temperatura foi "+ collestCityName + " com a temperatura de " + collestCityTemp);
     }
 }
