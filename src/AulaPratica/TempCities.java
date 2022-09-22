16cd12ec654ae9c82bca81fa4ddf7460c59c600e
magnovicentini <magno.vicentini@mercadolivre.com>
2022-09-22 16:52:41 -0300

    iniciando java

---
+++ src/AulaPratica/TempCities.java
@@ -0,0 +1,15 @@
+package AulaPratica;
+
+public class TempCities {
+    public static void main(String[] args) {
+        String[] cities = { "Londres", "Madrid", "Nueva York", "Buenos Aires",
+                "Asunción", "São Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
+        int[][] tempCities = { { -2, 33}, {-3, 32}, {-8, 27}, {4, 37}, {6, 42}, {5, 43}, {0, 39}, {-7, 26}, {-1, 31}, {-10, 35} };
+
+        System.out.println(cities[0]);
+
+        for (int i = 0; i < cities.length; i++) {
+            int hottestCity = [i]
+        }
+    }
+}
